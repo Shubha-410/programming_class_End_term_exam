21368966ddba4059fdf7120467fd13fba1d9b11e
Shubha Narayan <fsn214@nyu.edu>
2017-12-22 11:51:00 -0500

    Improved version 2.0

--- tetrahymena_part_A_fsn214.R
+++ tetrahymena_part_A_fsn214.R
@@ -1,4 +1,6 @@
 library(tidyverse)
 tetrahymena= read.csv('tetrahymena.tsv', sep='',header= T)
 tetrahymena= as_tibble(tetrahymena)
+tetrahymena= filter(tetrahymena, !(diameter <= 19.2))
+tetrahymena= group_by(tetrahymena,culture,glucose) 
 
