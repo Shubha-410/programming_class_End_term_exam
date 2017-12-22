72f0b9fd1fcfb80c6fcf636cbc51b6644f2a5d71
Shubha Narayan <fsn214@nyu.edu>
2017-12-22 11:55:32 -0500

    Improved version 3.0

--- tetrahymena_part_A_fsn214.R
+++ tetrahymena_part_A_fsn214.R
@@ -3,4 +3,6 @@ tetrahymena= read.csv('tetrahymena.tsv', sep='',header= T)
 tetrahymena= as_tibble(tetrahymena)
 tetrahymena= filter(tetrahymena, !(diameter <= 19.2))
 tetrahymena= group_by(tetrahymena,culture,glucose) 
+tetrahymena= summarise(tetrahymena, mean_conc= mean(conc),mean_dia= mean(diameter))
+tetrahymena= mutate(tetrahymena, log_conc= log(mean_conc), log_diameter= log(mean_dia))
 
