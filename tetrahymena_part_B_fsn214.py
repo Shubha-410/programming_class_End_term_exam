c2808430731fb7c4d6260c0141a32a1723e07c7b
Shubha Narayan <fsn214@nyu.edu>
2017-12-22 12:16:41 -0500

    Improved version 3.0

--- tetrahymena_part_B_fsn214.py
+++ tetrahymena_part_B_fsn214.py
@@ -10,4 +10,8 @@ df= tetrahymena[(tetrahymena.diameter) > 19.2]
 group_mean= df.groupby(['culture','glucose'])['conc','diameter'].mean() 
 group_mean['log_conc']= np.log(group_mean.conc)
 group_mean['log_diameter']= np.log(group_mean.diameter) 
+group_mean= group_mean.reset_index()
+plt.xlabel('log_conc')
+plt.ylabel('log_diameter')
+cluster= ['^','o']
 
